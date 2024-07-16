0cbb504aecd95d85169833de1e9b1a41dfa69342
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-07-16 23:37:38 +0700

    add color theme to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-navy-theme': '#2B293D',
+        'yellow-theme': '#FFE047'
+      }
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
