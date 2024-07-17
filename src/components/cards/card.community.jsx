50e65d33d1483baf13313bdcec7cdb5fb92235a1
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-07-17 21:25:14 +0700

    modify attribute community card

--- src/components/cards/card.community.jsx
+++ src/components/cards/card.community.jsx
@@ -1,4 +1,4 @@
-export const CommunityCard = () => {
+export const CommunityCard = ({name, city, sportType, isPrivate}) => {
   return (
     <div className="card card-compact bg-base-100 w-72 md:w-80 lg:w-96 shadow-xl">
       <figure className="md:h-40 lg:h-48">
@@ -7,9 +7,30 @@ export const CommunityCard = () => {
           alt="Sepeda"
         />
       </figure>
-      <div className="card-body gap-1">
-        <h2 className="card-title">Jakarta Fun Bicycle <div className="badge badge-outline">Private</div></h2>
-          <p className="w-fit">Sepeda | Jakarta</p>
+      <div className="card-body gap-0">
+        <div className="flex justify-between">
+          <a href="/" className="hover:underline">
+            <h2 className="card-title m-0">{name}</h2>
+          </a>
+          <button className="btn btn-circle btn-ghost btn-sm">
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 48 48"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M30 28.16C28.48 28.16 27.12 28.76 26.08 29.7L11.82 21.4C11.92 20.94 12 20.48 12 20C12 19.52 11.92 19.06 11.82 18.6L25.92 10.38C27 11.38 28.42 12 30 12C33.32 12 36 9.32 36 6C36 2.68 33.32 0 30 0C26.68 0 24 2.68 24 6C24 6.48 24.08 6.94 24.18 7.4L10.08 15.62C9 14.62 7.58 14 6 14C2.68 14 0 16.68 0 20C0 23.32 2.68 26 6 26C7.58 26 9 25.38 10.08 24.38L24.32 32.7C24.22 33.12 24.16 33.56 24.16 34C24.16 37.22 26.78 39.84 30 39.84C33.22 39.84 35.84 37.22 35.84 34C35.84 30.78 33.22 28.16 30 28.16Z"
+                fill="black"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="flex justify-between mt-1">
+          <p className="w-fit">{sportType} | {city}</p>
+          {isPrivate &&  <div className="badge badge-outline">Private</div>}
+        </div>
       </div>
     </div>
   );
