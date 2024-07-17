6a84fc1c5c4c6722e80c27a193279f4e72d758f2
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-07-17 21:24:31 +0700

    add page template to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,55 @@ import { Navbar } from "@/components/navbar";
 import { FilterCommunityBySportType } from "@/components/search-filter/filter.community.sport-type";
 import { SearchCommunityByCity } from "@/components/search-filter/search.community.city";
 import { CommunityCard } from "@/components/cards/card.community";
+import { PageTemplate } from "@/components/page-template";
 
 const userDummy = {
   id: 1,
   name: "Nadia",
 };
 
+const communityDummy = [
+  {
+    communityId: 1,
+    name: "Jakarta Fun Bicycle",
+    city: "Jakarta",
+    sportType: "Sepeda",
+    isPrivate: false
+  },
+  {
+    communityId: 2,
+    name: "Badminton Asyik",
+    city: "Bandung",
+    sportType: "Bulu tangkis",
+    isPrivate: true,
+  },
+  {
+    communityId: 3,
+    name: "Basket Keren",
+    city: "Semarang",
+    sportType: "Basket",
+    isPrivate: false
+  },
+  {
+    communityId: 4,
+    name: "Voli Ceria dan Bermanfaat",
+    city: "Surabaya",
+    sportType: "Voli",
+    isPrivate: true,
+  },
+  {
+    communityId: 5,
+    name: "Gym Gokil Mantap",
+    city: "Balikpapan",
+    sportType: "Gym",
+    isPrivate: false
+  }
+
+]
+
 export default function Home() {
   return (
-    <>
-      <header>
-        <Navbar user={userDummy} />
-      </header>
+    <PageTemplate>
       <main className="mt-6 container mx-auto p-4 md:p-6">
         <section className="flex flex-col md:flex-row justify-center items-center gap-2 lg:gap-4">
           <FilterCommunityBySportType />
@@ -23,14 +60,9 @@ export default function Home() {
           Temukan Komunitas Yang Sesuai Dengan Dirimu
         </h2>
         <section className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 justify-items-center">
-          <CommunityCard />
-          <CommunityCard />
-          <CommunityCard />
-          <CommunityCard />
-          <CommunityCard />
-          <CommunityCard />
+          {communityDummy.map(item => <CommunityCard name={item.name} city={item.city} sportType={item.sportType} isPrivate={item.isPrivate} key={item.communityId} />)}
         </section>
       </main>
-    </>
+    </PageTemplate>
   );
 }
