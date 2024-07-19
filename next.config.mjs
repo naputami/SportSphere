82e804868e275cc58d299212f452399b58e20610
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-07-19 20:42:18 +0700

    change config for loading image from R2 bucket

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       hostname: "", // url hostname of your images bucket
-  //       pathname: "/**",
-  //       protocol: "https",
-  //     },
-  //   ],
-  // },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "pub-cdf6484754244f30b7639332dce2c1b5.r2.dev", // url hostname of your images bucket
+        pathname: "/**",
+        protocol: "https",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
