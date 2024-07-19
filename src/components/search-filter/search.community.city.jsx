a0b0b8f5659024db09233cccfad5799fbb1f542a
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-07-19 20:45:57 +0700

    add search city  component

--- src/components/search-filter/search.community.city.jsx
+++ src/components/search-filter/search.community.city.jsx
@@ -1,8 +1,33 @@
+"use client";
+
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+
 export const SearchCommunityByCity = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { push } = useRouter();
+
+  const handleKeywordSubmit = (event) => {
+    event.preventDefault()
+    const params = new URLSearchParams(searchParams);
+
+    const formData = new FormData(event.target)
+
+    const keyword = formData.get("city")
+    if (keyword) {
+      params.set('city', keyword);
+    } else {
+      params.delete('city');
+    }
+
+    push(`${pathname}?${params.toString()}`);
+  }
+  
   return (
-    <form className="w-full max-w-xs">
+    <form className="w-full max-w-xs" onSubmit={handleKeywordSubmit}>
       <label className="input input-bordered flex items-center gap-2 w-full max-w-xs relative">
         <input
+        name="city"
           type="text"
           className="grow"
           placeholder="Cari komunitas berdasarkan kota"
