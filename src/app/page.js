6e5f3184ef43fb68796bb9045c6d93abbe8ba730
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-07-19 20:43:23 +0700

    add searchParams to home

--- src/app/page.js
+++ src/app/page.js
@@ -1,54 +1,13 @@
-import { Navbar } from "@/components/navbar";
 import { FilterCommunityBySportType } from "@/components/search-filter/filter.community.sport-type";
 import { SearchCommunityByCity } from "@/components/search-filter/search.community.city";
 import { CommunityCard } from "@/components/cards/card.community";
 import { PageTemplate } from "@/components/page-template";
+import { getAllPublicCommunities } from "@/services/community.service";
 
-const userDummy = {
-  id: 1,
-  name: "Nadia",
-};
+export default async function Home({ searchParams }) {
+  const { sportType, city } = searchParams;
+  const communities = await getAllPublicCommunities(sportType, city);
 
-const communityDummy = [
-  {
-    communityId: 1,
-    name: "Jakarta Fun Bicycle",
-    city: "Jakarta",
-    sportType: "Sepeda",
-    isPrivate: false
-  },
-  {
-    communityId: 2,
-    name: "Badminton Asyik",
-    city: "Bandung",
-    sportType: "Bulu tangkis",
-    isPrivate: true,
-  },
-  {
-    communityId: 3,
-    name: "Basket Keren",
-    city: "Semarang",
-    sportType: "Basket",
-    isPrivate: false
-  },
-  {
-    communityId: 4,
-    name: "Voli Ceria dan Bermanfaat",
-    city: "Surabaya",
-    sportType: "Voli",
-    isPrivate: true,
-  },
-  {
-    communityId: 5,
-    name: "Gym Gokil Mantap",
-    city: "Balikpapan",
-    sportType: "Gym",
-    isPrivate: false
-  }
-
-]
-
-export default function Home() {
   return (
     <PageTemplate>
       <main className="mt-6 container mx-auto p-4 md:p-6">
@@ -60,7 +19,17 @@ export default function Home() {
           Temukan Komunitas Yang Sesuai Dengan Dirimu
         </h2>
         <section className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 justify-items-center">
-          {communityDummy.map(item => <CommunityCard name={item.name} city={item.city} sportType={item.sportType} isPrivate={item.isPrivate} key={item.communityId} />)}
+          {communities.map((item) => (
+            <CommunityCard
+              name={item.name}
+              city={item.city}
+              sportType={item.sport_type}
+              isPrivate={item.is_private}
+              key={item.community_id}
+              imageTitle={item.community_image_profile}
+              communityId={item.community_id}
+            />
+          ))}
         </section>
       </main>
     </PageTemplate>
