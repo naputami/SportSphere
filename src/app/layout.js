068039872a7cd98a335e98e253e546f516341e7e
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-07-20 08:41:54 +0700

    add toaster to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 import "@/styles/globals.css";
+import { Toaster } from "react-hot-toast";
 
 export const metadata = {
   title:
@@ -15,7 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        <Toaster />
+        {children}
+      </body>
     </html>
   );
 }
