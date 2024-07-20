797aad23ccaceb4ba7b4e127a9347966f71129cb
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-07-20 08:40:30 +0700

    create footer for page template

---
+++ src/components/template/page-template.jsx
@@ -0,0 +1,20 @@
+import { Navbar } from "../navbar";
+import { serverAuth } from "@/libs/serverAuth";
+export const PageTemplate = ({ children }) => {
+  const user = serverAuth();
+  return (
+    <>
+      <div className="relative min-h-screen">
+        <div className="pb-16">
+          <Navbar user={user} />
+          {children}
+        </div>
+        <footer className="absolute bottom-0 footer footer-center p-3 bg-dark-navy-theme text-white">
+          <aside>
+            <p> © {new Date().getFullYear()} - Sport Sphere by Elysium</p>
+          </aside>
+        </footer>
+      </div>
+    </>
+  );
+};
