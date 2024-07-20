d30e0506929e69de930159a840b634876dccf430
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-07-20 14:48:32 +0700

    modify page.js for community detail

---
+++ src/app/community/[communityId]/page.js
@@ -0,0 +1,11 @@
+import { PageTemplate } from "@/components/template/page-template"
+
+export default function page() {
+  return (
+    <PageTemplate>
+      <main className="mt-6 container mx-auto p-4 md:p-6">
+        <div></div>
+      </main>
+    </PageTemplate>
+  )
+}
