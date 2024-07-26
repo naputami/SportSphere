f92093d74ba9b3a751c372bb67ec09211067cb48
myusea <kittenoceans@gmail.com>
2024-07-26 21:19:54 +0700

    change action in create community, still error

--- src/app/community/[communityId]/create/action.js
+++ src/app/community/[communityId]/create/action.js
@@ -1,19 +1,61 @@
 "use server";
 
-import { cookies } from "next/headers";
-import jwt from "jsonwebtoken";
+import { prisma } from "@/utils/prisma";
+import { uploadFile } from "@/libs/uploadFile";
 
-export function serverAuth() {
-  const token = cookies().get("token")?.value;
+export async function CreateCommunityAction(_, formData) {
+  const communityName = formData.get("communityName");
+  const sportType = formData.get("sportType");
+  const city = formData.get("city");
+  const file = formData.get("file");
+  const setPrivate = formData.get("setPrivate");
+  const communityDescription = formData.get("communityDescription");
 
-   if (!token) {
-     return null;
-   }
+  if (
+    !communityName ||
+    !sportType ||
+    !city ||
+    !file ||
+    !setPrivate ||
+    !communityDescription
+  ) {
+    return {
+      status: "error",
+      message: "All fields are required",
+    };
+  }
 
-   try {
-     const payload = jwt.verify(token, process.env.JWT_SECRET);
-     return payload;
-   } catch (error) {
-     return null;
-   }
+  if (
+    file.type !== "image/jpg" &&
+    file.type !== "image/png" &&
+    file.type !== "image/jpeg"
+  ) {
+    return {
+      status: "error",
+      message: "File extension is not allowed",
+    };
+  }
+
+  const community = await prisma.community.create({
+    data: {
+      name: communityName,
+      city: city,
+      sport_type: sportType,
+      description: communityDescription,
+      community_image_profile: file.name,
+      is_private: setPrivate === "yes",
+    },
+  });
+
+  await uploadFile({
+    key: file.name,
+    body: file,
+    folder: `communities/${community.community_id}`,
+  });
+
+  return {
+    status: "success",
+    message: `Community is successfully created as ${setPrivate === "yes" ? "private" : "public"
+      }`,
+  };
 }
