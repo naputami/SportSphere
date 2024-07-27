2d045d001a439a261c779dd159ce2bcfa096c1ed
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-07-27 17:56:14 +0700

    fix form community, add dropdown for city and sport type

--- src/components/formCommunity.jsx
+++ src/components/formCommunity.jsx
@@ -1,22 +1,18 @@
 "use client";
 
-import { CreateCommunity } from "@/app/community/create/action"
+import { CreateCommunity } from "@/app/community/create/action";
 import { useEffect, useState } from "react";
 import { useActionState } from "react";
 import toast from "react-hot-toast";
 import { Button } from "@/components/button";
 import Link from "next/link";
+import { sportTypes } from "@/libs/getDisplayedSportType";
 
-export const FormCommunity = ({userId}) => {
+export const FormCommunity = ({ userId }) => {
   const [state, formAction, pending] = useActionState(CreateCommunity, null);
-  const [formData, setFormData] = useState({
-    communityName: "",
-    sportType: "",
-    city: "",
-    file: null,
-    setPrivate: "",
-    communityDescription: "",
-  });
+  const [provinces, setProvinces] = useState([]);
+  const [selectedProvince, setSelectedProvince] = useState("");
+  const [cities, setCities] = useState([]);
 
   useEffect(() => {
     if (state?.status === "success") {
@@ -26,18 +22,28 @@ export const FormCommunity = ({userId}) => {
     }
   }, [state]);
 
-  const handleChange = (e) => {
-    const { name, value, files } = e.target;
-    if (files) {
-      setFormData((prevData) => ({ ...prevData, [name]: files[0] }));
-    } else {
-      setFormData((prevData) => ({ ...prevData, [name]: value }));
+  useEffect(() => {
+    // Fetch provinces
+    fetch("https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json")
+      .then((response) => response.json())
+      .then((data) => setProvinces(data))
+      .catch((error) => console.error("Error fetching provinces:", error));
+  }, []);
+
+  useEffect(() => {
+    if (selectedProvince) {
+      fetch(
+        `https://www.emsifa.com/api-wilayah-indonesia/api/regencies/${selectedProvince}.json`
+      )
+        .then((response) => response.json())
+        .then((data) => setCities(data))
+        .catch((error) => console.error("Error fetching cities:", error));
     }
-  };
+  }, [selectedProvince]);
 
   return (
     <div className="bg-gray-100 flex items-center justify-center min-h-screen p-4">
-       <form
+      <form
         action={formAction}
         className="bg-white px-5 py-5 rounded-lg shadow-md w-full max-w-2xl m-4"
       >
@@ -55,7 +61,6 @@ export const FormCommunity = ({userId}) => {
         </h2>
         <div className="m-auto max-w-xl space-y-2">
           <h3 className="text-lg font-medium">Community Details</h3>
-
           <label className="form-control w-full">
             <div className="label">
               <span className="label-text">Community name</span>
@@ -64,10 +69,8 @@ export const FormCommunity = ({userId}) => {
             <input
               type="text"
               name="communityName"
-              placeholder="Type here"
+              placeholder="Type community name here"
               className="input input-bordered input-md w-full"
-              value={formData.communityName}
-              onChange={handleChange}
             />
           </label>
 
@@ -76,17 +79,19 @@ export const FormCommunity = ({userId}) => {
               <span className="label-text">Sport Type</span>
               <span className="text-red-500 ml-1">*</span>
             </div>
-            <input
-              type="text"
-              name="sportType"
-              placeholder="Type here"
-              className="input input-bordered input-md w-full"
-              value={formData.sportType}
-              onChange={handleChange}
-            />
+            <select className="select select-bordered w-full" name="sportType">
+              <option disabled selected>
+                Select Sport Type
+              </option>
+              {sportTypes.map((item) => (
+                <option key={item.value} value={item.value}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
           </label>
 
-          <label className="form-control w-full">
+          {/* <label className="form-control w-full">
             <div className="label">
               <span className="label-text">City</span>
               <span className="text-red-500 ml-1">*</span>
@@ -96,9 +101,47 @@ export const FormCommunity = ({userId}) => {
               name="city"
               placeholder="Type here"
               className="input input-bordered input-md w-full"
-              value={formData.city}
-              onChange={handleChange}
             />
+          </label> */}
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Province</span>
+              <span className="text-red-500 ml-1">*</span>
+            </div>
+            <select
+              className="select select-bordered w-full"
+              onChange={(e) => setSelectedProvince(e.target.value)}
+            >
+              <option disabled selected>
+                Select Province
+              </option>
+              {provinces.map((province) => (
+                <option key={province.id} value={province.id}>
+                  {province.name}
+                </option>
+              ))}
+            </select>
+          </label>
+
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">City</span>
+              <span className="text-red-500 ml-1">*</span>
+            </div>
+            <select
+              className="select select-bordered w-full"
+              disabled={!selectedProvince}
+              name="city"
+            >
+              <option disabled selected>
+                Select City
+              </option>
+              {cities.map((item) => (
+                <option key={item.id} value={item.name}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
           </label>
 
           <label className="form-control w-full">
@@ -111,7 +154,6 @@ export const FormCommunity = ({userId}) => {
               name="file"
               accept=".jpg,.jpeg,.png"
               className="file-input file-input-bordered w-full"
-              onChange={handleChange}
             />
           </label>
 
@@ -127,7 +169,6 @@ export const FormCommunity = ({userId}) => {
                   name="setPrivate"
                   value="yes"
                   className="radio radio-md"
-                  onChange={handleChange}
                 />
                 <span>Yes</span>
               </label>
@@ -137,7 +178,6 @@ export const FormCommunity = ({userId}) => {
                   name="setPrivate"
                   value="no"
                   className="radio radio-md"
-                  onChange={handleChange}
                 />
                 <span>No</span>
               </label>
@@ -153,23 +193,16 @@ export const FormCommunity = ({userId}) => {
               type="text"
               name="communityDescription"
               placeholder="Describe what's special about your community and other important details"
-              className="input input-bordered input-md w-full"
-              value={formData.communityDescription}
-              onChange={handleChange}
+              className="textarea textarea-bordered textarea-md w-full"
             />
           </label>
         </div>
         <input type="hidden" name="userId" value={userId} />`
-
         <div className="flex justify-end mt-4">
           <Button variant="secondary" type="submit" disabled={pending}>
+            {pending && <span className="loading loading-spinner"></span> }
             Submit
           </Button>
-          {state && (
-            <p className={state.status === "success" ? "text-green-500" : "text-red-500"}>
-              {state.message}
-            </p>
-          )}
         </div>
       </form>
     </div>
